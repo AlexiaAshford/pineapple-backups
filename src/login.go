e855568f5436a9d3a390e558147426498be125d4
Alex <luanyin4444@qq.com>
2022-07-12 17:36:42 +0800

    ~

--- src/login.go
+++ src/login.go
@@ -20,12 +20,11 @@ func AccountDetailed() string {
 func LoginAccount(username string, password string) {
 	CookieArray, status := boluobao.Post_login_by_account(username, password)
 	if status.Status.HTTPCode == 200 {
-		cfg.Load()
-		CookieMap := make(map[string]string)
+		cfg.Var.Cookie = ""
 		for _, cookie := range CookieArray {
-			CookieMap[cookie.Name] = cookie.Value
+			cfg.Var.Cookie += cookie.Name + "=" + cookie.Value + ";"
 		}
-		cfg.Var.Cookie, cfg.Var.UserName, cfg.Var.Password = CookieMap, username, password
+		cfg.Var.UserName, cfg.Var.Password = username, password
 		cfg.SaveJson()
 		fmt.Println(AccountDetailed(), "\tLogin successful!")
 	} else {
