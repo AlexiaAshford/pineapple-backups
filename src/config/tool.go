e6a7d27ac03c4d2dbf27210951eb886cd824708c
Alex <luanyin4444@qq.com>
2022-07-12 17:40:39 +0800

    ~

--- src/config/tool.go
+++ src/config/tool.go
@@ -1,9 +1,19 @@
 package config
 
 import (
+	"fmt"
+	"os"
+	"path"
 	"regexp"
 )
 
 func RegexpName(Name string) string {
 	return regexp.MustCompile(`[\\/:*?"<>|]`).ReplaceAllString(Name, "")
+}
+
+func Mkdir(filePath string) {
+	dirPath, _ := os.Getwd()
+	if err := os.MkdirAll(path.Join(dirPath, filePath), os.ModePerm); err != nil {
+		fmt.Println(err)
+	}
 }
