4ed5c264045b3585a078265e17cc3c1bea72537d
Alex <luanyin4444@qq.com>
2022-07-14 19:40:37 +0800

    ~

---
+++ src/boluobao/urlconstant.go
@@ -0,0 +1,10 @@
+package boluobao
+
+const (
+	BookDetailedById        = "novels/%v?expand="
+	AccountDetailedByApi    = "user"
+	CatalogueDetailedById   = "novels/%v/dirs?expand=originNeedFireMoney"
+	ContentDetailedByCid    = "Chaps/%v?expand=content&autoOrder=true"
+	SearchDetailedByKeyword = "search/novels/result?q=%v&size=20&page=0&expand="
+	LoginByAccount          = "sessions"
+)
