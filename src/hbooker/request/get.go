e2fb397285bcd7fad4292217a709bb6a8a95b4c1
Alex <luanyin4444@qq.com>
2022-07-15 15:20:11 +0800

    ~

--- src/hbooker/request/get.go
+++ src/hbooker/request/get.go
@@ -1,1 +1,24 @@
 package request
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func Get(url string) []byte {
+	api := fmt.Sprintf("%v", url)
+	if req, err := http.NewRequest("GET", api, nil); err == nil {
+		SetHeaders(req, true)
+		if resp, ok := client.Do(req); ok == nil {
+			if bodyText, ReadBody := ioutil.ReadAll(resp.Body); ReadBody == nil {
+				return bodyText
+			} else {
+				fmt.Println("Read Body Error:", ReadBody)
+			}
+		} else {
+			fmt.Println("Get error:", err)
+		}
+	}
+	return nil
+}
