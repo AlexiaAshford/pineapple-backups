6a53ed50fd5df5b0bb5394edaeb6d477894f1ce3
Alex <luanyin4444@qq.com>
2022-07-23 10:12:36 +0800

    ~

--- src/hbooker/decode.go
+++ src/hbooker/decode.go
@@ -8,56 +8,38 @@ import (
 	"fmt"
 )
 
-var (
-	//IV 偏移量
-	IV = []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-)
-
 //SHA256 sha256 编码
 func SHA256(data []byte) []byte {
 	ret := sha256.Sum256(data)
 	return ret[:]
 }
 
-//Base64Decode Base64 解码
-func Base64Decode(encoded string) ([]byte, error) {
-	if decoded, err := base64.StdEncoding.DecodeString(encoded); err == nil {
-		return decoded, nil
-	} else {
-		return nil, err
-	}
-}
-
-//LoadKey 读取解密密钥
-func LoadKey(EncryptKey string) []byte {
-	Key := SHA256([]byte(EncryptKey))
-	return Key[:32]
-}
-
 //AESDecrypt AES 解密
-func AESDecrypt(EncryptKey string, ciphertext []byte) ([]byte, error) {
-	if EncryptKey == "" {
-		EncryptKey = "zG2nSeEfSHfvTCHy5LCcqtBbQehKNLXn"
-	}
-	if block, err := aes.NewCipher(LoadKey(EncryptKey)); err == nil {
-		blockModel, plainText := cipher.NewCBCDecrypter(block, IV), make([]byte, len(ciphertext))
-		blockModel.CryptBlocks(plainText, ciphertext)
-		return plainText[:(len(plainText) - int(plainText[len(plainText)-1]))], nil
+func AESDecrypt(EncryptKey string, contentText string) ([]byte, error) {
+	if decoded, err := base64.StdEncoding.DecodeString(contentText); err == nil {
+		if block, ok := aes.NewCipher(SHA256([]byte(EncryptKey))[:32]); ok == nil {
+			blockModel, plainText := cipher.NewCBCDecrypter(block,
+				[]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}), make([]byte, len(decoded))
+			blockModel.CryptBlocks(plainText, decoded)
+			return plainText[:(len(plainText) - int(plainText[len(plainText)-1]))], nil
+		} else {
+			fmt.Println("AESDecrypt Error:", ok)
+			return nil, ok
+		}
 	} else {
+		fmt.Println("Base64Decode Error:", err)
 		return nil, err
 	}
 }
 
 //Decode 入口函数
 func Decode(content string, EncryptKey string) []byte {
-	if decoded, err := Base64Decode(content); err == nil {
-		if raw, ok := AESDecrypt(EncryptKey, decoded); ok == nil {
-			return raw
-		} else {
-			fmt.Println("AESDecrypt Error:", ok)
-		}
-	} else {
-		fmt.Println("Base64Decode Error:", err)
+	if EncryptKey == "" {
+		EncryptKey = "zG2nSeEfSHfvTCHy5LCcqtBbQehKNLXn"
 	}
-	panic("Decrypt Error, Please Check Your Key!")
+	if raw, ok := AESDecrypt(EncryptKey, content); ok == nil {
+		return raw
+	} else {
+		panic("Decrypt Error, Please Check Your Key!")
+	}
 }
