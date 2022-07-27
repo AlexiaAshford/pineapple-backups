1d15bc6dae1eab63193e1330ce5ab0848dfef38a
VeronicaAlexia <luanyin4444@qq.com>
2022-07-27 20:59:04 +0800

    ~

--- src/catalogue.go
+++ src/catalogue.go
@@ -2,8 +2,6 @@ package src
 
 import (
 	"fmt"
-	"math/rand"
-	"os"
 	"path"
 	"sf/cfg"
 	"sf/src/boluobao"
@@ -11,7 +9,6 @@ import (
 	"sf/structural/hbooker_structs"
 	"sf/structural/sfacg_structs"
 	"strconv"
-	"time"
 )
 
 type Catalogue struct {
@@ -22,51 +19,67 @@ type Catalogue struct {
 	ChapterCfg   string
 }
 
-func (catalogue *Catalogue) ReadChapterConfig() {
-	catalogue.ConfigPath = path.Join(cfg.Vars.ConfigFile, cfg.Vars.BookInfo.NovelName+".conf")
-	catalogue.SaveTextPath = path.Join(cfg.Vars.SaveFile, cfg.Vars.BookInfo.NovelName+".txt")
+func (catalogue *Catalogue) ReadChapterConfig() string {
+	catalogue.ConfigPath = path.Join(cfg.Vars.ConfigFile, cfg.BookConfig.BookInfo.NovelName+".conf")
+	catalogue.SaveTextPath = path.Join(cfg.Vars.SaveFile, cfg.BookConfig.BookInfo.NovelName+".txt")
+	return cfg.Vars.AppType
 }
 
-func (catalogue *Catalogue) SfacgCatalogue() bool {
-	catalogue.ReadChapterConfig()
-	response := boluobao.GetCatalogueDetailedById(cfg.Vars.BookInfo.NovelID)
-	for divisionIndex, division := range response.Data.VolumeList {
-		fmt.Printf("第%v卷\t\t%v\n", divisionIndex+1, division.Title)
-		for _, chapter := range division.ChapterList {
-			if chapter.OriginNeedFireMoney == 0 {
-				catalogue.ChapterList = append(catalogue.ChapterList, strconv.Itoa(chapter.ChapID))
-			} else {
+func (catalogue *Catalogue) InitCatalogue() {
+	switch catalogue.ReadChapterConfig() {
+	case "sfacg":
+		for divisionIndex, division := range boluobao.GetCatalogue(cfg.BookConfig.BookInfo.NovelID).Data.VolumeList {
+			fmt.Printf("第%v卷\t\t%v\n", divisionIndex+1, division.Title)
+			for _, chapter := range division.ChapterList {
 				if chapter.OriginNeedFireMoney == 0 {
-					fmt.Println(chapter.Title, "已经下载过了")
+					catalogue.ChapterList = append(catalogue.ChapterList, strconv.Itoa(chapter.ChapID))
 				}
 			}
-
 		}
+	case "cat":
+		for index, division := range hbooker.GetDivisionIdByBookId(cfg.BookConfig.BookInfo.NovelID) {
+			fmt.Printf("第%v卷\t\t%v\n", index+1, division.DivisionName)
+			for _, chapter := range hbooker.GetCatalogueByDivisionId(division.DivisionID) {
+				if chapter.IsValid == "1" {
+					catalogue.ChapterList = append(catalogue.ChapterList, chapter.ChapterID)
+				}
+			}
+		}
+
 	}
-	catalogue.ChapterBar = New(len(catalogue.ChapterList))
-	catalogue.ChapterBar.Describe("进度:")
-	for _, ChapID := range catalogue.ChapterList {
-		catalogue.SfacgContent(ChapID)
+	if len(catalogue.ChapterList) > 0 {
+		catalogue.ChapterBar = New(len(catalogue.ChapterList))
+		catalogue.ChapterBar.Describe("进度:")
+		catalogue.DownloadContent()
+		fmt.Printf("\nNovelName:%vdownload complete!", cfg.BookConfig.BookInfo.NovelName)
+	} else {
+		fmt.Println("No chapter need to download!")
 	}
-	catalogue.ChapterList = nil
-	return true
 }
 
-func (catalogue *Catalogue) SfacgContent(ChapterId string) {
-	for i := 0; i < 5; i++ {
-		response := boluobao.GetContentDetailedByCid(ChapterId)
-		if response.Status.HTTPCode == 200 {
-			catalogue.makeContentInformation(response)
-			break
-		} else {
-			if response.Status.Msg == "接口校验失败,请尽快把APP升级到最新版哦~" {
-				fmt.Println(response.Status.Msg)
-				os.Exit(0)
+func (catalogue *Catalogue) DownloadContent() {
+	for _, ChapterId := range catalogue.ChapterList {
+		for i := 0; i < 5; i++ {
+			if cfg.Vars.AppType == "sfacg" {
+				response := boluobao.GetContentDetailedByCid(ChapterId)
+				if response.Status.HTTPCode == 200 {
+					catalogue.makeContentInformation(response)
+					break
+				} else {
+					fmt.Println("error message:", response.Status.Msg)
+				}
+			} else if cfg.Vars.AppType == "cat" {
+				response := hbooker.GetContent(ChapterId)
+				if response.Code == "100000" {
+					catalogue.makeContentInformation(response)
+					break
+				}
 			} else {
-				fmt.Println(response.Status.Msg)
+				panic("app type error please check config file")
 			}
 		}
 	}
+	catalogue.ChapterList = nil
 }
 
 func (catalogue *Catalogue) makeContentInformation(response any) {
@@ -86,42 +99,10 @@ func (catalogue *Catalogue) makeContentInformation(response any) {
 
 }
 
-func (catalogue *Catalogue) CatCatalogue() bool {
-	catalogue.ReadChapterConfig()
-	for index, division := range hbooker.GetDivisionIdByBookId(cfg.Vars.BookInfo.NovelID) {
-		fmt.Printf("第%v卷\t\t%v\n", index+1, division.DivisionName)
-		for _, chapter := range hbooker.GetCatalogueByDivisionId(division.DivisionID) {
-			if chapter.IsValid == "1" {
-				catalogue.ChapterList = append(catalogue.ChapterList, chapter.ChapterID)
-			} else {
-				if chapter.IsValid == "1" {
-					fmt.Println(chapter.ChapterTitle, "已经下载过了")
-				}
-			}
-		}
-	}
-	catalogue.ChapterBar = New(len(catalogue.ChapterList))
-	for _, ChapterID := range catalogue.ChapterList {
-		catalogue.CatContent(ChapterID)
-	}
-	catalogue.ChapterList = nil
-	return true
-}
-
-func (catalogue *Catalogue) CatContent(ChapterId string) {
-	for i := 0; i < 5; i++ {
-		response := hbooker.GetContent(ChapterId)
-		if response.Code == "100000" {
-			catalogue.makeContentInformation(response)
-			break
-		}
-	}
-
-}
 func (catalogue *Catalogue) SpeedProgressAndDelayTime() {
 	if err := catalogue.ChapterBar.Add(1); err != nil {
 		fmt.Println("bar error:", err)
 	} else {
-		time.Sleep(time.Second * time.Duration(rand.Intn(2)))
+		//time.Sleep(time.Second * time.Duration(rand.Intn(2)))
 	}
 }
