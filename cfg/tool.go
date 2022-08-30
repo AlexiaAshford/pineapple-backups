7772a15fcfea456a8387bbe447738138ed068cc2
VeronicaAlexia <Elaina-Alex@proton.me>
2022-08-30 14:46:30 +0800

    ~

--- cfg/tool.go
+++ cfg/tool.go
@@ -86,10 +86,20 @@ func ColorPrint(s string, i int) {
 }
 
 func ExtractBookID(url string) string {
-	if url != "" {
-		bookID := regexp.MustCompile(`(\d+)`).FindStringSubmatch(url)
-		if len(bookID) > 1 {
-			return bookID[1]
+	current_book_id := regexp.MustCompile(`(\d+)`).FindStringSubmatch(url)
+	if len(current_book_id) > 1 {
+		if Vars.AppType == "sfacg" {
+			if len(current_book_id[1]) < 5 {
+				fmt.Println("book_id is invalid")
+			} else {
+				return current_book_id[1]
+			}
+		} else if Vars.AppType == "cat" {
+			if len(current_book_id[1]) != 9 { // test if the input is hbooker book id
+				fmt.Println("hbooker bookid is 9 characters, please input again:")
+			} else {
+				return current_book_id[1]
+			}
 		}
 	}
 	return ""
